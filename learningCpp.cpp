5962018dc147c0217bef78a7f5717660ffedd1c6
Derek Isensee <derek.isensee@gmail.com>
2016-10-24 07:34:03 -0500

    Git ignore update

---
+++ learningCpp.cpp
@@ -0,0 +1,17 @@
+// learningCpp.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello" << std::endl;
+	/* Two different ways of printing a new line. std::endl is probably better.
+	std::cout << std::endl;
+	std::cout << "\n";
+	*/
+
+	return 0;
+}
+
